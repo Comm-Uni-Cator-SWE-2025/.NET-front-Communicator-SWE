d2465163b5fb0debe3d42264b7e8169e1fc84a1a
Logan <112201016@smail.iitpkd.ac.in>
2025-11-07 11:02:11 +0530

    Networking front for c#

---
+++ Networking/ClientNode.cs
@@ -0,0 +1,8 @@
+﻿/**
+ * A simple data class to hold client information (hostname and port).
+ * This is the Java equivalent of a tuple(string, port).
+ * @param hostName The IP address.
+ * @param port The PORT.
+ */
+namespace Networking;
+public record ClientNode(string HostName, int Port);
---
+++ Networking/INetworking.cs
@@ -0,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking;
+/**
+ * Interface used between other modules and networking to send data.
+ * Every module subscribes to this interface and then sends data
+ * using the sendData function
+ */
+public interface INetworking
+{
+    /**
+     * Function to send data to given list of destination.
+     *
+     * @param data     the data to be sent
+     * @param dest     the destination to send the data
+     * @param module   the module to send to
+     * @param priority the priority of the data
+     */
+    void SendData(byte[] data, ClientNode[] dest, int module, int priority);
+
+    /**
+     * Function to send data to all clients.
+     *
+     * @param data     the data to be sent
+     * @param module   the module to be sent to
+     * @param priority the priority of the data
+     */
+    void Broadcast(byte[] data, int module, int priority);
+
+    /**
+     * Function to subscribe a function to the network.
+     *
+     * @param name     the name of the module.
+     * @param function the function to invoke on receiving the packet
+     */
+    void Subscribe(int name, IMessageListener function);
+
+    /**
+     * Functiont to remove from subscription.
+     *
+     * @param name the name of the module
+     */
+    void RemoveSubscription(int name);
+}
