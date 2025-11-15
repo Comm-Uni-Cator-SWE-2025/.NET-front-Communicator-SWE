782fc73a8d3f25e45b2c85e8280007e20bf188b2
Bhupathi Varun <142201034@smail.iitpkd.ac.in>
2025-11-16 01:38:36 +0530

    modified communicator.sln and added ScreenShare.UX

---
+++ src/ScreenShare.UX/Controls/ParticipantControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ScreenShare.UX.Controls
+{
+    /// <summary>
+    /// Interaction logic for ParticipantControl.xaml
+    /// </summary>
+    public partial class ParticipantControl : UserControl
+    {
+        public static readonly DependencyProperty InitialProperty =
+            DependencyProperty.Register("Initial", typeof(string), typeof(ParticipantControl), 
+                new PropertyMetadata("Y", OnInitialChanged));
+
+        public static readonly DependencyProperty UsernameProperty =
+            DependencyProperty.Register("Username", typeof(string), typeof(ParticipantControl), 
+                new PropertyMetadata("You", OnUsernameChanged));
+
+        public string Initial
+        {
+            get { return (string)GetValue(InitialProperty); }
+            set { SetValue(InitialProperty, value); }
+        }
+
+        public string Username
+        {
+            get { return (string)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+
+        public ParticipantControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnInitialChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ParticipantControl;
+            if (control != null)
+            {
+                control.InitialTextBlock.Text = e.NewValue?.ToString() ?? "Y";
+            }
+        }
+
+        private static void OnUsernameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ParticipantControl;
+            if (control != null)
+            {
+                control.UsernameTextBlock.Text = e.NewValue?.ToString() ?? "You";
+            }
+        }
+    }
+}
---
+++ src/ScreenShare.UX/MeetingView.xaml.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenShare.UX;
+/// <summary>
+/// Interaction logic for MeetingView.xaml
+/// </summary>
+public partial class MeetingView : UserControl
+{
+
+    public ObservableCollection<ParticipantData> Participants { get; set; }
+    private int participantCounter = 1; // Counter for generating participant names
+
+    public MeetingView()
+    {
+
+        InitializeComponent();
+    }
+}
