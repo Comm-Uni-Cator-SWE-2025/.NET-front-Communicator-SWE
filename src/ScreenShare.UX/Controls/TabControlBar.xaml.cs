06bff153996a438464e0d6b70f4f4edd304ec6f3
Bhupathi Varun <142201034@smail.iitpkd.ac.in>
2025-11-16 17:31:35 +0530

    added .\Controls\TabControlBar*

---
+++ src/ScreenShare.UX/Controls/TabControlBar.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WpfApp1.Controls
+{
+    /// <summary>
+    /// Interaction logic for TabControlBar.xaml
+    /// </summary>
+    public partial class TabControlBar : UserControl
+    {
+        public TabControlBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
