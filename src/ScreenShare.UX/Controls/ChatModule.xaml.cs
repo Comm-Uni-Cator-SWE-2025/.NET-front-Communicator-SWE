a76b2e8076206b81daed109e03888d999992895b
Bhupathi Varun <142201034@smail.iitpkd.ac.in>
2025-11-16 17:30:33 +0530

    added .\Controls\ChatModule*

---
+++ src/ScreenShare.UX/Controls/ChatModule.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WpfApp1.Controls
+{
+    /// <summary>
+    /// Interaction logic for ChatModule.xaml
+    /// </summary>
+    public partial class ChatModule : UserControl
+    {
+        public ChatModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
