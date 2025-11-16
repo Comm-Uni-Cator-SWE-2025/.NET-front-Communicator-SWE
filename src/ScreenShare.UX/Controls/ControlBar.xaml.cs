75f1ae5adfb73a4de68d49de8832a0460d03a538
Bhupathi Varun <142201034@smail.iitpkd.ac.in>
2025-11-16 17:31:05 +0530

    added .\Controls\ControlBar*

---
+++ src/ScreenShare.UX/Controls/ControlBar.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WpfApp1.Controls
+{
+    /// <summary>
+    /// Interaction logic for ControlBar.xaml
+    /// </summary>
+    public partial class ControlBar : UserControl
+    {
+        public ControlBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
