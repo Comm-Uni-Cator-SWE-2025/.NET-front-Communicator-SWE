c006e67c02ba7ffbfc029c396c215f889ac6929b
udithdoddapaneni <udithdoddapaneni@gmail.com>
2025-11-17 13:28:22 +0530

    Networking RPC--using abstract RPC

---
+++ Communicator.App/IAbstractRPC.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Communicator.App;
+
+public interface IAbstractRPC
+{
+    void Subscribe(string methodName, Func<byte[], byte[]> method);
+
+    Thread Connect(int portNumber);
+
+    Task<byte[]> Call(string methodName, byte[] data);
+}
+
