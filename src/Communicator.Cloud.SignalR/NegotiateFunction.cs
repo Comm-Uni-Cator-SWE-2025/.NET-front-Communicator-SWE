a5f9df5c39cabea704e6add03b7a94e5ea025d7f
Nikhil S Thomas <nikhil.s.thomas5@gmail.com>
2025-11-18 17:20:13 +0530

    Added comments for all files for SignalR Function App

--- src/Communicator.Cloud.SignalR/NegotiateFunction.cs
+++ src/Communicator.Cloud.SignalR/NegotiateFunction.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿/******************************************************************************
+* Filename    = NegotiateFunction.cs
+* Author      = Nikhil S Thomas
+* Product     = Comm-Uni-Cator
+* Project     = SignalR Function App
+* Description = Azure Function to handle SignalR negotiation requests.
+*****************************************************************************/
+
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.SignalRService;
@@ -7,15 +15,31 @@ using System.Net;
 
 namespace Communicator.Cloud.SignalR;
 
+/// <summary>
+/// Class to handle SignalR negotiation requests.
+/// </summary>
 public class NegotiateFunction
 {
+    /// <summary>
+    /// Logger instance for logging information.
+    /// </summary>
     private readonly ILogger<NegotiateFunction> _logger;
 
+    /// <summary>
+    /// Constructor to initialize the logger.
+    /// </summary>
+    /// <param name="logger">Used to instantiate logger</param>
     public NegotiateFunction(ILogger<NegotiateFunction> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// Function app endpoint to handle negotiation requests.
+    /// </summary>
+    /// <param name="req">HTTP Request</param>
+    /// <param name="connectionInfo">Auto-generated SignalR Connection Info</param>
+    /// <returns>HTTP response with connection info (URL and Access Token)</returns>
     [Function("negotiate")]
     public async Task<HttpResponseData> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
--- src/Communicator.Cloud.SignalR/Program.cs
+++ src/Communicator.Cloud.SignalR/Program.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿/******************************************************************************
+* Filename    = Program.cs
+* Author      = Nikhil S Thomas
+* Product     = Comm-Uni-Cator
+* Project     = SignalR Function App
+* Description = Auto-generated Program entry point for the Azure Functions application.
+*****************************************************************************/
+
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+/// <summary>
+/// Auto-generated Program entry point for the Azure Functions application.
+/// </summary>
 internal class Program
 {
+    /// <summary>
+    /// Main method to configure and run the Functions application.
+    /// </summary>
+    /// <param name="args"></param>
     private static void Main(string[] args)
     {
         FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
